c7566222f17e82eb059e902a205367991691b263
Fikry Farenza <vickyfarenza@gmail.com>
2022-04-01 15:13:39 +0700

    Dynamic List :art:

--- app/src/main/java/fryctze/college/course/MainActivity.kt
+++ app/src/main/java/fryctze/college/course/MainActivity.kt
@@ -2,6 +2,7 @@ package fryctze.college.course
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import fryctze.college.course.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +15,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        loadList()
+    }
+
+    private fun loadList() {
+        // Items of listview
+        val item: ArrayList<String> = ArrayList()
+        item.add("Android")
+        item.add("Windows")
+        item.add("iOS")
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            item)
+
+        binding.opsys.adapter = adapter
+
+        item.add("Linux")
     }
 }
